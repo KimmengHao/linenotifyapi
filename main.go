7e638d541117cc6dd0973b4a5d5e646b3b3a89fc
Arthit Kim <arthit@imake.co.th>
2021-06-20 11:55:47 +0700

    testapi

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -16,8 +15,6 @@ import (
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
-	"github.com/utahta/go-linenotify/auth"
-	"github.com/utahta/go-linenotify/token"
 	"google.golang.org/api/iterator"
 )
 
@@ -35,44 +32,48 @@ type numsResponseData struct {
 }
 
 func Authorize(w http.ResponseWriter, req *http.Request) {
-	c, err := auth.New(ClientID, BaseURL+"/callback")
-	if err != nil {
-		fmt.Fprintf(w, "error:%v", err)
-		return
-	}
-	http.SetCookie(w, &http.Cookie{Name: "state", Value: c.State, Expires: time.Now().Add(60 * time.Second)})
+	// c, err := auth.New(ClientID, BaseURL+"/callback")
+	// if err != nil {
+	// 	fmt.Fprintf(w, "error:%v", err)
+	// 	return
+	// }
+	// http.SetCookie(w, &http.Cookie{Name: "state", Value: c.State, Expires: time.Now().Add(60 * time.Second)})
 
-	c.Redirect(w, req)
+	// c.Redirect(w, req)
+	fmt.Fprintf(w, "token:authorize")
+
 }
 
 func Callback(w http.ResponseWriter, req *http.Request) {
-	resp, err := auth.ParseRequest(req)
-	if err != nil {
-		fmt.Fprintf(w, "error:%v", err)
-		return
-	}
+	// resp, err := auth.ParseRequest(req)
+	// if err != nil {
+	// 	fmt.Fprintf(w, "error:%v", err)
+	// 	return
+	// }
 
-	state, err := req.Cookie("state")
-	if err != nil {
-		fmt.Fprintf(w, "error:%v", err)
-		return
-	}
-	if resp.State != state.Value {
-		fmt.Fprintf(w, "error:%v", err)
-		return
-	}
+	// state, err := req.Cookie("state")
+	// if err != nil {
+	// 	fmt.Fprintf(w, "error:%v", err)
+	// 	return
+	// }
+	// if resp.State != state.Value {
+	// 	fmt.Fprintf(w, "error:%v", err)
+	// 	return
+	// }
 
-	c := token.NewClient(BaseURL+"/callback", ClientID, ClientSecret)
-	accessToken, err := c.GetAccessToken(context.Background(), resp.Code)
-	if err != nil {
-		fmt.Fprintf(w, "error:%v", err)
-		return
-	}
-	adddata(accessToken)
+	// c := token.NewClient(BaseURL+"/callback", ClientID, ClientSecret)
+	// accessToken, err := c.GetAccessToken(context.Background(), resp.Code)
+	// if err != nil {
+	// 	fmt.Fprintf(w, "error:%v", err)
+	// 	return
+	// }
+	// adddata(accessToken)
 
-	// notify(accesstoken)
+	// // notify(accesstoken)
 
-	fmt.Fprintf(w, "token:%v", accessToken)
+	// fmt.Fprintf(w, "token:%v", accessToken)
+	fmt.Fprintf(w, "token:callback")
+
 }
 
 func adddata(accessToken string) {
